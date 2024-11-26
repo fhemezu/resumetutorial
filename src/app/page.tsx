caecd0ee36abe53652355cde3f149de2482eb53b
Faheem <Faheem.zura1@gmail.com>
2024-11-26 15:43:15 +0800

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { FiDownload } from "react-icons/fi";
+// import { Button } from "@/components/ui/button";
+// import { FiDownload } from "react-icons/fi";
 
 const Home = () => {
   return (
